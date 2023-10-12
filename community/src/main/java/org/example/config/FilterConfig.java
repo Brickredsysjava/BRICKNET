28fcb4e7afec92723c137a74b960739fd3a020a1
Pankaj <pp843958@gmail.com>
2023-10-12 11:23:58 +0530

    12th oct 1st commit

--- community/src/main/java/org/example/config/FilterConfig.java
+++ community/src/main/java/org/example/config/FilterConfig.java
@@ -1,17 +1,17 @@
-package org.example.config;
-
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class FilterConfig {
-
-    @Bean
-    public FilterRegistrationBean<AuthorizationFilter> tokenFilter() {
-        FilterRegistrationBean<AuthorizationFilter> registrationBean = new FilterRegistrationBean<>();
-        registrationBean.setFilter(new AuthorizationFilter());
-        registrationBean.addUrlPatterns("/communityPost/*"); // Set the URL pattern for your API endpoints
-        return registrationBean;
-    }
-}
+//package org.example.config;
+//
+//import org.springframework.boot.web.servlet.FilterRegistrationBean;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class FilterConfig {
+//
+//    @Bean
+//    public FilterRegistrationBean<AuthorizationFilter> tokenFilter() {
+//        FilterRegistrationBean<AuthorizationFilter> registrationBean = new FilterRegistrationBean<>();
+//        registrationBean.setFilter(new AuthorizationFilter());
+//        registrationBean.addUrlPatterns("/communityPost/*"); // Set the URL pattern for your API endpoints
+//        return registrationBean;
+//    }
+//}
