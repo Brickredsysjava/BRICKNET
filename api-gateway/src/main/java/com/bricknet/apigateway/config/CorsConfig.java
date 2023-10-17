63fcc3032e842ff7b5b887f5fe849378d3c2f31f
Pankaj <pp843958@gmail.com>
2023-10-17 15:40:46 +0530

    changes in Jwt map

--- api-gateway/src/main/java/com/bricknet/apigateway/config/CorsConfig.java
+++ api-gateway/src/main/java/com/bricknet/apigateway/config/CorsConfig.java
@@ -9,17 +9,30 @@ import org.springframework.web.cors.reactive.CorsWebFilter;
 @Configuration
 public class CorsConfig {
 
-    @Bean
-    public CorsWebFilter corsWebFilter() {
-        CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.applyPermitDefaultValues();
-        corsConfig.addAllowedMethod("*");
-        corsConfig.addAllowedHeader("*");
-        corsConfig.addAllowedOrigin("*");
-        
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", corsConfig);
+//    @Bean
+//    public CorsWebFilter corsWebFilter() {
+//        CorsConfiguration corsConfig = new CorsConfiguration();
+//        corsConfig.applyPermitDefaultValues();
+//        corsConfig.addAllowedMethod("*");
+//        corsConfig.addAllowedHeader("*");
+//        corsConfig.addAllowedOrigin("*");
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", corsConfig);
+//
+//        return new CorsWebFilter(source);
+//    }
 
-        return new CorsWebFilter(source);
-    }
+        @Bean
+        public CorsWebFilter corsFilter() {
+            CorsConfiguration corsConfig = new CorsConfiguration();
+            corsConfig.addAllowedOrigin("*");
+            corsConfig.addAllowedMethod("*");
+            corsConfig.addAllowedHeader("*");
+
+            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+            source.registerCorsConfiguration("/**", corsConfig);
+
+            return new CorsWebFilter(source);
+        }
 }
