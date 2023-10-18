11b90762095fbb9ebfdcab9c54816a7c4bacb6a4
Pankaj <pp843958@gmail.com>
2023-10-18 12:28:10 +0530

    changes for web client

--- auth-server/src/main/java/com/bricknet/authserver/FeignClient/Notification.java
+++ auth-server/src/main/java/com/bricknet/authserver/FeignClient/Notification.java
@@ -1,14 +1,29 @@
 package com.bricknet.authserver.FeignClient;
 
+import com.bricknet.authserver.Dto.ForgetPassword;
 import com.bricknet.authserver.Dto.NotificationDto;
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import com.bricknet.authserver.Dto.UserAuthInfo;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
-@FeignClient("notification/send")
-public interface Notification {
-    @PostMapping("/email")
-    public ResponseEntity<String> sendEmailNotification(
-            @RequestBody NotificationDto notification);
+@Component
+public class Notification {
+
+    private WebClient.Builder webClientBuilder;
+
+    public Notification(WebClient.Builder webClientBuilder) {
+        this.webClientBuilder = webClientBuilder.baseUrl("http://192.168.1.9:8084/send");
+    }
+
+    public void sendEmailNotification(NotificationDto notificationDto){
+        webClientBuilder.build().post().uri(uriBuilder -> uriBuilder.path("/email").build())
+                .bodyValue(notificationDto).retrieve().bodyToMono(NotificationDto.class);
+    }
+
+
+
+//    @PostMapping("/email")
+//    public ResponseEntity<String> sendEmailNotification(
+//            @RequestBody NotificationDto notification);
 }
