801ff818fa98c3c66411c070c073889e4ad830be
kabiraneja <kabiraneja021@gmail.com>
2023-10-19 12:28:28 +0530

    cors update

--- api-gateway/src/main/java/com/bricknet/apigateway/config/CorsConfig.java
+++ api-gateway/src/main/java/com/bricknet/apigateway/config/CorsConfig.java
@@ -1,37 +1,30 @@
-//package com.bricknet.apigateway.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.bind.annotation.CrossOrigin;
-//import org.springframework.web.cors.CorsConfiguration;
-//import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-//import org.springframework.web.cors.reactive.CorsWebFilter;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//@CrossOrigin(origins = "*")
-//public class CorsConfig implements WebMvcConfigurer {
-//
-////    @Bean
-////    public CorsWebFilter corsWebFilter() {
-////        CorsConfiguration corsConfig = new CorsConfiguration();
-////        corsConfig.applyPermitDefaultValues();
-////        corsConfig.addAllowedMethod("*");
-////        corsConfig.addAllowedHeader("*");
-////        corsConfig.addAllowedOrigin("*");
-////
-////        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-////        source.registerCorsConfiguration("/**", corsConfig);
-////
-////        return new CorsWebFilter(source);
-////    }
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins("http://192.168.1.35:3000/")
-//                .allowedMethods("GET", "POST", "PUT", "DELETE")
-//                .allowedHeaders("Origin","Content-Type","Accept","Authorization","Referer");
-//    }
-//}
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@CrossOrigin(origins = "*")
+public class CorsConfig {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("http://192.168.1.35:3000"); // Replace with the origin of your frontend app
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        return new CorsFilter(corsConfigurationSource());
+    }
+}
