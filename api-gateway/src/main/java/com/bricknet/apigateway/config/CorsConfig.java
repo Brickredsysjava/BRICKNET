87488aa1a5408fc3df382f294fe357e35be5772c
kabiraneja <kabiraneja021@gmail.com>
2023-10-19 11:38:37 +0530

    cors error update

--- api-gateway/src/main/java/com/bricknet/apigateway/config/CorsConfig.java
+++ api-gateway/src/main/java/com/bricknet/apigateway/config/CorsConfig.java
@@ -32,6 +32,6 @@ public class CorsConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("Origin","Content-Type","Accept","Authorization","Referer");
+//                .allowedHeaders("Origin","Content-Type","Accept","Authorization","Referer");
     }
 }
