57811d13a3d940136b19adf305a4b251c47588e3
kabiraneja <kabiraneja021@gmail.com>
2023-10-26 17:25:20 +0530

    new commit

--- notification/src/main/java/com/notification/Notification/Dto/NotificationDTO.java
+++ notification/src/main/java/com/notification/Notification/Dto/NotificationDTO.java
@@ -1,5 +1,9 @@
 package com.notification.Notification.Dto;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,6 +16,16 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 public class NotificationDTO {
+
+
+    @Valid
+    @NotNull(message = "message can't be null")
+    @NotBlank(message = "message can't be blank")
+    @NotEmpty(message = "message can't be empty")
     private String message;
+
+
+    @Valid
+    @NotNull(message = "timestamp can't be null")
     private LocalDateTime timestamp;
 }
