60e2e4f0d3b5a73c4a1ceadd3bde9c4d6dfc3e0b
kabiraneja <kabiraneja021@gmail.com>
2023-10-27 11:55:52 +0530

    new commit

--- SuperAdmin/src/main/java/com/example/SuperAdmin/DTO/BankDetailsDTO.java
+++ SuperAdmin/src/main/java/com/example/SuperAdmin/DTO/BankDetailsDTO.java
@@ -1,5 +1,6 @@
 package com.example.SuperAdmin.DTO;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
@@ -14,24 +15,30 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class BankDetailsDTO {
+
+    @Valid
     @NotBlank(message = "Account number is required")
     @Size(min = 8, max = 20, message = "Account number should be between 8 and 20 characters")
     @NotNull(message = "account number is required")
     private String accountNumber;
 
+    @Valid
     @NotBlank(message = "Bank name is required")
     @NotNull(message = "Bank name is required")
     private String bankName;
 
+    @Valid
     @NotBlank(message = "Branch name is required")
     @NotNull(message = "Branch name is required")
     private String branchName;
 
+    @Valid
     @NotBlank(message = "IFSC code is required")
     @Pattern(regexp = "^[A-Z]{4}[0-9]{7}$", message = "Invalid IFSC code format")
     @NotNull(message = "IFSC code is required")
     private String ifscCode;
 
+    @Valid
     @NotBlank(message = "PAN number is required")
     @Pattern(regexp = "[A-Z]{5}[0-9]{4}[A-Z]{1}", message = "Invalid PAN number format")
     @NotNull(message = "PAN number is required")
--- SuperAdmin/src/main/java/com/example/SuperAdmin/DTO/PersonalDetailsDTO.java
+++ SuperAdmin/src/main/java/com/example/SuperAdmin/DTO/PersonalDetailsDTO.java
@@ -1,5 +1,6 @@
 package com.example.SuperAdmin.DTO;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -12,34 +13,43 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class PersonalDetailsDTO {
+
+    @Valid
     @NotBlank(message = "Date of birth is required")
     @NotNull(message = "Date of birth is required")
     private String dateOfBirth;
 
+    @Valid
     @NotBlank(message = "Nationality is required")
     @NotNull(message = "nationality is required")
     private String nationality;
 
+    @Valid
     @NotBlank(message = "Place of birth is required")
     @NotNull(message = "Place of birth is required")
     private String placeOfBirth;
 
+    @Valid
     @NotBlank(message = "Religion is required")
     @NotNull(message = "Religion is required")
     private String religion;
 
+    @Valid
     @NotBlank(message = "Father's name is required")
     @NotNull(message = "Father's name is required")
     private String fatherName;
 
+    @Valid
     @NotBlank(message = "International employee status is required")
     @NotNull(message = "International employee status is required")
     private String internationalEmployee;
 
+    @Valid
     @NotBlank(message = "Marital status is required")
     @NotNull(message = "Marital status is required")
     private String maritalStatus;
 
+    @Valid
     @NotBlank(message = "Physically challenged status is required")
     @NotNull(message = "Physically challenged status is required")
     private String physicallyChallenged;
