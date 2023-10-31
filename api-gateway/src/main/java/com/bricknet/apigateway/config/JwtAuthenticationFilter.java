fd324171162f386c7fe90a2611723f71db3b74c9
Pankaj <pp843958@gmail.com>
2023-10-31 14:31:32 +0530

    change in jwt authentication filter

--- api-gateway/src/main/java/com/bricknet/apigateway/config/JwtAuthenticationFilter.java
+++ api-gateway/src/main/java/com/bricknet/apigateway/config/JwtAuthenticationFilter.java
@@ -48,8 +48,8 @@ public class JwtAuthenticationFilter implements WebFilter{
             log.warn("This is empcode ---------------------");
             log.warn(empcode);
 
-            //String comparedJwtInJWTMap = String.valueOf(jwtMap.getByjwt(empcode));
-            String comparedJwtInJWTMap = redisService.get(empcode);
+            String comparedJwtInJWTMap = String.valueOf(jwtMap.getByjwt(empcode));
+            //String comparedJwtInJWTMap = redisService.get(empcode);
             log.warn("This is comparedJwtInJwtMap   ---------------------");
             log.warn(comparedJwtInJWTMap);
             if (comparedJwtInJWTMap != null) {
