1f82fa33700b634c5c7ecb14222307738f208d56
Pankaj Pandey <72627485+Pankaj-sudo419@users.noreply.github.com>
2023-11-02 15:11:03 +0530

    Delete attendance directory

--- attendance/src/main/java/com/attendanceApiForApp/attendanceApiForApp/dto/HistoryDto.java
+++
@@ -1,9 +0,0 @@
-package com.attendanceApiForApp.attendanceApiForApp.dto;
-
-public class HistoryDto {
-    private  String date;
-    private  String firstCheckin;
-    private  String lastCheckout;
-    private  String durations;
-    private  String timezone;
-}
--- attendance/src/main/java/com/attendanceApiForApp/attendanceApiForApp/dto/TransactionDto.java
+++
@@ -1,7 +0,0 @@
-package com.attendanceApiForApp.attendanceApiForApp.dto;
-
-public class TransactionDto {
-    private  String checkIn_times;
-    private  String checkOut_times;
-    private  String durations;
-}
