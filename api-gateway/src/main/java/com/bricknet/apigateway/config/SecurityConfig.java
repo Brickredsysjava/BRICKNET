fc54aeaa05feb735262edf4651bdfc01fe12c221
kabiraneja <kabiraneja021@gmail.com>
2023-11-17 12:47:17 +0530

    new commit

--- api-gateway/src/main/java/com/bricknet/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/bricknet/apigateway/config/SecurityConfig.java
@@ -30,9 +30,9 @@ public class SecurityConfig {
                     .csrf(ServerHttpSecurity.CsrfSpec::disable)
                     .cors(ServerHttpSecurity.CorsSpec::disable)
                     .authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec
-                            .pathMatchers("/eureka/**","super-admin/swagger-ui/index.html","/auth/getOtp/**","/auth/checkOtp/**","/auth/login/**" , "/auth/**").permitAll()
-                            .pathMatchers("/user/**","/community/post/**" ,"/send/**", "user/profile/profileFromUserName/**" , "/api/broadcasting/**", "/api/**", "/media/**", "/suggestionPost/api/**").permitAll()
-                            .pathMatchers("/communityPost/admin/**").hasAuthority("ADMIN")
+                            .pathMatchers("/eureka/**","super-admin/swagger-ui/index.html", "/auth/**").permitAll()
+                            .pathMatchers("/user/**","/community/post/**" ,"/send/**", "user/profile/profileFromUserName/**" , "/api/broadcasting/**", "/api/to-do/**", "/media/**", "/suggestionPost/api/suggestions/**").permitAll()
+                            .pathMatchers("/suggestionPost/api/verification/**","/communityPost/admin/**").hasAuthority("ADMIN")
                             .anyExchange().authenticated())
                     .addFilterAt(jwtAuthenticationFilter, SecurityWebFiltersOrder.AUTHENTICATION)
                     .build();
