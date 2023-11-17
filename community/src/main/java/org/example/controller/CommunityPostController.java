8343910f491daf33413e7c31bb1371fb3a342d83
kabiraneja <kabiraneja021@gmail.com>
2023-11-17 12:17:46 +0530

    new commit

--- community/src/main/java/org/example/controller/CommunityPostController.java
+++ community/src/main/java/org/example/controller/CommunityPostController.java
@@ -20,7 +20,7 @@ import java.util.List;
 
 @CrossOrigin("*")
 @RestController
-@RequestMapping("/communityPost")
+@RequestMapping("/community/post")
 public class CommunityPostController {
 
 @Autowired
