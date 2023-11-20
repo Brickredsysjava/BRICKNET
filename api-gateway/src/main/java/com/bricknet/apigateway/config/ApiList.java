ed10298e256a794f9a5b8cda25211d0f2e360948
kabiraneja <kabiraneja021@gmail.com>
2023-11-20 14:57:29 +0530

    new commit

--- api-gateway/src/main/java/com/bricknet/apigateway/config/ApiList.java
+++ api-gateway/src/main/java/com/bricknet/apigateway/config/ApiList.java
@@ -9,12 +9,59 @@ public class ApiList {
 
 
     static String[] admin = new String[] {
+//            "/user/profile/addProfile/**",
+//            "/user/profile/addListOfProfile/**",
+//            "/user/profile/allProfile",
+//            "/suggestionPost/api/verification/**",
+//            "/api/broadcasting/**",
+//            "/communityPost/admin/getAllPostNeedToVerified",
+//            "/communityPost/admin/postVerification/**",
+//            "/user/personal/updatePersonalDetails/**",
+//            "/user/personal/addPersonalDetails/**",
+//            "/user/personal/addListOfPersonalDetails/**",
+//            "/user/personal/allPersonalDetails",
+//            "/user/education/updateEducation/**",
+//            "/user/education/addListOfEducation/**",
+//            "/user/education/addEducation/**",
+//            "/user/education/allEducation",
+//            "/user/education/deleteEducation/**",
+//            "/user/bank/updateBankDetails/**",
+//            "/user/bank/addListOfBankDetails/**",
+//            "/user/bank/addBankDetails/**",
+//            "/user/address/updateAddress/**",
+//            "/user/address/addListOfAddress/**",
+//            "/user/address/addAddress/**",
+//            "/user/address/test",
+//            "/user/address/allAddress",
+//            "/user/address/deleteAddress/**",
+//            "/user/creation/**"
+    };
+
+    static String[] openApi = new String[] {
+            "/eureka/**",
+            "/auth/**",
+            "/communityPost/post/**",
+            "/suggestionPost/api/suggestions/**",
+            "/send/**",
+            "/api/to-do/**",
+            "/media/**",
+            "/user/profile/passwordUpdate/**",
+            "/user/profile/timeLine/**",
+            "/user/profile/getEmailByEmployeeCode/**",
+            "/user/profile/fullName",
+            "/user/profile/ProfileById/**",
+            "/user/profile/updateProfile/**",
+            "/user/personal/PersonalDetailsById/**",
+            "/user/education/EducationById/**",
+            "/user/bank/bankDetailsById/**",
+            "/user/address/AddressById/**",
+
             "/user/profile/addProfile/**",
             "/user/profile/addListOfProfile/**",
             "/user/profile/allProfile",
             "/suggestionPost/api/verification/**",
             "/api/broadcasting/**",
-//            "/communityPost/admin/getAllPostNeedToVerified",
+            "/communityPost/admin/getAllPostNeedToVerified",
             "/communityPost/admin/postVerification/**",
             "/user/personal/updatePersonalDetails/**",
             "/user/personal/addPersonalDetails/**",
@@ -35,26 +82,6 @@ public class ApiList {
             "/user/address/allAddress",
             "/user/address/deleteAddress/**",
             "/user/creation/**"
-    };
-
-    static String[] openApi = new String[] {
-            "/eureka/**",
-            "/auth/**",
-            "/communityPost/post/**",
-            "/suggestionPost/api/suggestions/**",
-            "/send/**",
-            "/api/to-do/**",
-            "/media/**",
-            "/user/profile/passwordUpdate/**",
-            "/user/profile/timeLine/**",
-            "/user/profile/getEmailByEmployeeCode/**",
-            "/user/profile/fullName",
-            "/user/profile/ProfileById/**",
-            "/user/profile/updateProfile/**",
-            "/user/personal/PersonalDetailsById/**",
-            "/user/education/EducationById/**",
-            "/user/bank/bankDetailsById/**",
-            "/user/address/AddressById/**"
     };
 
 //    public static final ArrayList<String> adminApiList = (ArrayList<String>) Arrays.asList(admin);
