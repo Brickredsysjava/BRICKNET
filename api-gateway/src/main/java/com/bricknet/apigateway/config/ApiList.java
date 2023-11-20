42dd2f8642d6e715af8b94b258cce02d4d754932
kabiraneja <kabiraneja021@gmail.com>
2023-11-20 13:20:16 +0530

    new commit

--- api-gateway/src/main/java/com/bricknet/apigateway/config/ApiList.java
+++ api-gateway/src/main/java/com/bricknet/apigateway/config/ApiList.java
@@ -8,11 +8,12 @@ import java.util.List;
 public class ApiList {
 
 
-    static String[] admin = new String[] {"/user/profile/addProfile/**", "/user/profile/addListOfProfile/**", "/suggestionPost/api/verification/**", "/api/broadcasting/**", "/communityPost/admin/**"};
-
-    static String[] openApi = new String[] {"/eureka/**","super-admin/swagger-ui/index.html", "/auth/**",
-            "/communityPost/post/**", "/send/**", "/api/to-do/**", "/media/**", "/suggestionPost/api/suggestions/**"
+    static String[] admin = new String[] {"/user/profile/addProfile/**", "/user/profile/addListOfProfile/**", "/user/profile/allProfile",
+            "/suggestionPost/api/verification/**", "/api/broadcasting/**", "/communityPost/admin/getAllPostNeedToVerified", "/communityPost/admin/postVerification/**",
+            "/user/profile/updateProfile/**", "/user/profile/passwordUpdate",
     };
+
+    static String[] openApi = new String[] {"/eureka/**", "/auth/**", "/communityPost/post/**", "/suggestionPost/api/suggestions/**", "/send/**", "/api/to-do/**", "/media/**"};
 
 //    public static final ArrayList<String> adminApiList = (ArrayList<String>) Arrays.asList(admin);
 //
