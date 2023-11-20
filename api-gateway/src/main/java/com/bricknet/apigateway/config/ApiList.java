d2204bd62997a672f6f0af13a2bd77327f422ec1
kabiraneja <kabiraneja021@gmail.com>
2023-11-20 13:57:42 +0530

    new commit

--- api-gateway/src/main/java/com/bricknet/apigateway/config/ApiList.java
+++ api-gateway/src/main/java/com/bricknet/apigateway/config/ApiList.java
@@ -8,12 +8,54 @@ import java.util.List;
 public class ApiList {
 
 
-    static String[] admin = new String[] {"/user/profile/addProfile/**", "/user/profile/addListOfProfile/**", "/user/profile/allProfile",
-            "/suggestionPost/api/verification/**", "/api/broadcasting/**", "/communityPost/admin/getAllPostNeedToVerified", "/communityPost/admin/postVerification/**",
-            "/user/profile/updateProfile/**", "/user/profile/passwordUpdate",
+    static String[] admin = new String[] {
+            "/user/profile/addProfile/**",
+            "/user/profile/addListOfProfile/**",
+            "/user/profile/allProfile",
+            "/suggestionPost/api/verification/**",
+            "/api/broadcasting/**",
+            "/communityPost/admin/getAllPostNeedToVerified",
+            "/communityPost/admin/postVerification/**",
+            "/user/personal/updatePersonalDetails/**",
+            "/user/personal/addPersonalDetails/**",
+            "/user/personal/addListOfPersonalDetails/**",
+            "/user/personal/allPersonalDetails",
+            "/user/education/updateEducation/**",
+            "/user/education/addListOfEducation/**",
+            "/user/education/addEducation/**",
+            "/user/education/allEducation",
+            "/user/education/deleteEducation/**",
+            "/user/bank/updateBankDetails/**",
+            "/user/bank/addListOfBankDetails/**",
+            "/user/bank/addBankDetails/**",
+            "/user/address/updateAddress/**",
+            "/user/address/addListOfAddress/**",
+            "/user/address/addAddress/**",
+            "/user/address/test",
+            "/user/address/allAddress",
+            "/user/address/deleteAddress/**",
+            "/user/creation/**"
     };
 
-    static String[] openApi = new String[] {"/eureka/**", "/auth/**", "/communityPost/post/**", "/suggestionPost/api/suggestions/**", "/send/**", "/api/to-do/**", "/media/**"};
+    static String[] openApi = new String[] {
+            "/eureka/**",
+            "/auth/**",
+            "/communityPost/post/**",
+            "/suggestionPost/api/suggestions/**",
+            "/send/**",
+            "/api/to-do/**",
+            "/media/**",
+            "/user/profile/passwordUpdate/**",
+            "/user/profile/timeLine/**",
+            "/user/profile/getEmailByEmployeeCode/**",
+            "/user/profile/fullName",
+            "/user/profile/ProfileById/**",
+            "/user/profile/updateProfile/**",
+            "/user/personal/PersonalDetailsById/**",
+            "/user/education/EducationById/**",
+            "/user/bank/bankDetailsById/**",
+            "/user/address/AddressById/**"
+    };
 
 //    public static final ArrayList<String> adminApiList = (ArrayList<String>) Arrays.asList(admin);
 //
