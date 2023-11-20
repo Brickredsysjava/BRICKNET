45af532a9acd390290f6e12f9a9b876817c0ee9f
kabiraneja <kabiraneja021@gmail.com>
2023-11-20 11:31:17 +0530

    new commit

---
+++ api-gateway/src/main/java/com/bricknet/apigateway/config/ApiList.java
@@ -0,0 +1,21 @@
+package com.bricknet.apigateway.config;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ApiList {
+
+
+    static String[] admin = new String[] {"/suggestionPost/api/verification/**","/communityPost/admin/**"};
+
+    static String[] openApi = new String[] {"/eureka/**","super-admin/swagger-ui/index.html", "/auth/**",
+            "/user/**","/communityPost/post/**", "/send/**", "user/profile/profileFromUserName/**" , "/api/broadcasting/**", "/api/to-do/**", "/media/**", "/suggestionPost/api/suggestions/**"
+    };
+
+//    public static final ArrayList<String> adminApiList = (ArrayList<String>) Arrays.asList(admin);
+//
+//    public static final ArrayList<String> openApiList = (ArrayList<String>) Arrays.asList(openApi);
+
+}
