80ea3cab13baa75d51d6cecbcf820ae83ed5380e
Ranjeet <102139258+0ranjeet@users.noreply.github.com>
2023-11-24 10:37:35 +0000

    getall

--- suggestion/src/main/java/com/example/suggestion/Service/SuggestionServiceImplementation.java
+++ suggestion/src/main/java/com/example/suggestion/Service/SuggestionServiceImplementation.java
@@ -107,7 +107,7 @@ public class SuggestionServiceImplementation implements SuggestionService{
             }).toList();
 
             newList.sort(Comparator.comparing(GetSuggestionsDTO::getSuggestionDateTime).reversed());
-
+            System.out.println(newList);
             return newList;
         
     }
