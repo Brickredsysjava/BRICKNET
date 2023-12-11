ef551a359bed68eea73d156ff64e26f0e8a20ca0
kabiraneja <kabiraneja021@gmail.com>
2023-12-11 17:27:06 +0530

    New Feature Group Creation

--- SuperAdmin/src/main/java/com/example/SuperAdmin/DTO/GroupDTO.java
+++ SuperAdmin/src/main/java/com/example/SuperAdmin/DTO/GroupDTO.java
@@ -1,6 +1,16 @@
 package com.example.SuperAdmin.DTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.List;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 
 public class GroupDTO {
     private String groupName;
