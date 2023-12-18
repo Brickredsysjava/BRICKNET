7cdd16df968793df75351769e5d3b139f1852e14
Pankaj <pp843958@gmail.com>
2023-12-18 12:31:49 +0530

    GetNewsletter

--- broadcast/src/main/java/com/microservices/Broadcasting/Controller/broadCastController.java
+++ broadcast/src/main/java/com/microservices/Broadcasting/Controller/broadCastController.java
@@ -126,12 +126,12 @@ public class broadCastController {
     }
 
     @GetMapping("/getNewsLetter")
-    public ResponseEntity<?> getNewsLetter(){
+    public ResponseEntity<List<BroadCastingDTO>> getNewsLetter(){
         List<BroadCastingDTO> broadcastingList = broadCastingService1.getNewsLetter();
         if(broadcastingList.get(0).getText() != null){
             return new ResponseEntity<>(broadcastingList, HttpStatus.OK);
         }
-        return new ResponseEntity<>("Not found", HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(broadcastingList, HttpStatus.NOT_FOUND);
     }
 
 }
