11712d8d51f06941b9555b1e746127bd06604786
Pankaj <pp843958@gmail.com>
2024-01-15 16:08:57 +0530

    192.168.0.9 changed to 192.168.1.9

--- auth-server/src/main/java/com/bricknet/authserver/FeignClient/UserProfile.java
+++ auth-server/src/main/java/com/bricknet/authserver/FeignClient/UserProfile.java
@@ -18,7 +18,7 @@ public class UserProfile {
     private final WebClient.Builder webClientBuilder;
 
     public UserProfile(WebClient.Builder webClientBuilder) {
-        this.webClientBuilder = webClientBuilder.baseUrl("http://192.168.0.9:8081");
+        this.webClientBuilder = webClientBuilder.baseUrl("http://192.168.1.9:8081");
     }
 
 //    public void UserProfileWebClient(WebClient.Builder webClientBuilder) {
